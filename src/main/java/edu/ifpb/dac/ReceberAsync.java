553acde828f55f4c36d339df07191d0d6a591250
Ricardo Job <sousajob@gmail.com>
2015-02-02 01:29:48 -0300

    Projeto inicializado

--- src/main/java/edu/ifpb/dac/ReceberAsync.java
+++ src/main/java/edu/ifpb/dac/ReceberAsync.java
@@ -2,6 +2,7 @@ package edu.ifpb.dac;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.ejb.ActivationConfigProperty;
 import javax.ejb.MessageDriven;
 import javax.jms.JMSException;
 import javax.jms.Message;
@@ -13,6 +14,12 @@ import javax.jms.TextMessage;
  * @author Ricardo Job
  */
 @MessageDriven(mappedName = "java:global/jms/demoQueue")
+//@MessageDriven(
+//        mappedName = "java:global/jms/demoQueue",
+//        activationConfig = {
+//            @ActivationConfigProperty(propertyName = "destinationType",
+//                    propertyValue = "javax.jms.Queue")
+//        })
 public class ReceberAsync implements MessageListener {
 
     @Override
@@ -21,7 +28,7 @@ public class ReceberAsync implements MessageListener {
             TextMessage tm = (TextMessage) message;
             System.out.println("Mensagem Recebida: " + tm.getText());
         } catch (JMSException ex) {
-//            Logger.getLogger(ReceberAsync.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(ReceberAsync.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 }
